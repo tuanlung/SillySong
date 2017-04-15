f00bb8e9566e96b44f588f71efd0641108108933
tuanlung <tuanlung@g.ucla.edu>
2017-04-15 13:22:51 -0700

    Silly song UI constraints and view controller logic. New features: 1. Clear name field and lyric board when starting entering name 2. Show lyrics combined with input name

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var nameField: UITextField!
+  @IBOutlet weak var lyricsView: UITextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    nameField.delegate = self
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +23,52 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func reset(_ sender: Any) {
+    nameField.text = String("")
+    lyricsView.text = String("")
+  }
+  
+  @IBAction func displayLyrics(_ sender: Any) {
+    lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+  }
 
 }
 
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return false
+  }
+}
+
+let bananaFanaTemplate = [
+  "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+  "Banana Fana Fo F<SHORT_NAME>",
+  "Me My Mo M<SHORT_NAME>",
+  "<FULL_NAME>"].joined(separator: "\n")
+
+func shortNameForName(name: String) -> String {
+  // implementation here
+  var name = name
+  name = name.lowercased()
+  
+  let vowels = "aeiou"
+  let characterSet = CharacterSet(charactersIn: vowels)
+  let range = name.rangeOfCharacter(from: characterSet)
+  let lowerBound = range?.lowerBound
+  if (lowerBound != nil) {
+    name = name.substring(from: lowerBound!)
+  }
+  return name
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+  
+  let shortName = shortNameForName(name: fullName)
+  
+  let lyrics = lyricsTemplate
+    .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+    .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+  
+  return lyrics
+}
